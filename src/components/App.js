ff436ad36c17a53529467ffa434177c51b8cfcca
Kelvin <kmwendwa476@gmail.com>
2022-07-25 00:06:14 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
-// create your App component here
+//create your App component here
+
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogData, setdogData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => setdogData(data.message));
+  }, []);
+
+  if (!dogData) {
+    return <p>Loading...</p>;
+  } else {
+    return (
+        <img src={dogData} alt="A Random Dog" />
+    );
+  }
+}
+
+export default App;
